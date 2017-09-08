37eb3ca8a6bd360e30f42c2649342f816e9119f6
JohnPlayerSpecial <cs.prio30@gmail.com>
2017-09-08 14:02:36 +0200

    Update tele.py

--- tele.py
+++ tele.py
@@ -36,7 +36,7 @@ def start(bot, update):
     if str( update.message.chat_id )  in MY_CHAT_ID.split(","):
         surplus = "\nYou are in the whitelist and can proceed to use the bot!"
     else:
-	surplus = "\nERROR: You are NOT in the whitelist and hence can not proceed to use the bot!"
+        surplus = "\nERROR: You are NOT in the whitelist and hence can not proceed to use the bot!"
     text = "Welcome.\n\nUse /search to search for PDFs." + surplus
     update.message.reply_text(text)
 
@@ -269,8 +269,8 @@ def main():
     updater = Updater(TOKEN_TELEGRAM) #ITEbooksBot
     dp = updater.dispatcher
     updater.dispatcher.add_handler(CommandHandler('start', start)))
-    updater.dispatcher.add_handler(CommandHandler('random', randomBook, filters = Filters.user(MY_CHAT_ID.split(",")) ))
-    updater.dispatcher.add_handler(RegexHandler('^/info_\d+', moreInfo, filters = Filters.user(MY_CHAT_ID.split(","))))
+    updater.dispatcher.add_handler(CommandHandler('random', randomBook, filters = Filters.user(MY_CHAT_ID.split(",") ) ))
+    updater.dispatcher.add_handler(RegexHandler('^/info_\d+', moreInfo, filters = Filters.user(MY_CHAT_ID.split(",") )))
   
     updater.dispatcher.add_handler(CallbackQueryHandler( editMessageSendMoreResults, pattern = '^EDITMESSAGEpage=' ))
     
